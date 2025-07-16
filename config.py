76ad3567f0f7e85002ce1d72c2336efa5ef1c8a9
Shanaia D'Silva <shanaia.dsilva30@gmail.com>
2025-07-16 13:48:10 +0530

    config added

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-class Config{
+class Config:
     SECRET_KEY=os.environ.get("SESSION_SECRET", "shanaia-dev-key-Baghirathi!@#$!#^@%^%#$&")
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  
     UPLOAD_FOLDER = 'uploads'
     OSRM_SERVER = os.environ.get('OSRM_SERVER', 'http://localhost:5000')
-}
+
+
